d74a12a1498c1b20250c8b807de2f7fa5df10f94
陈俊杰 <chenjunjie@singleronbio.com>
2025-08-25 16:25:50 +0800

    Add Median of TSS enrichment score

--- celescope/atac/analysis.py
+++ celescope/atac/analysis.py
@@ -1,13 +1,16 @@
 import subprocess
-import pandas as pd 
+import snapatac2 as snap
+import pandas as pd
+import numpy as np
 import math
+import os
 from celescope.tools.plotly_plot import Peak_plot, Tsne_plot, Frag_dis_plot
 from celescope.tools import utils
 from celescope.tools.step import Step, s_common
 
 
 def get_opts_analysis(parser, sub_program):
-
+    parser.add_argument('--reference', help='Genome reference directory includes fasta, index, promoter bed file.', required=True)
     if sub_program:
         s_common(parser)
         parser.add_argument('--analysis_dir', help='analysis directory', required=True)
@@ -22,12 +25,14 @@ class Analysis(Step):
     def __init__(self, args, display_title=None):
         Step.__init__(self, args, display_title=display_title)
         
+        self.reference =  os.path.abspath(args.reference)
         self.filtered_peak_count = args.filtered_peak_count
         self.analysis_dir = args.analysis_dir
+        self.fragment = f"{self.analysis_dir}/fragments_corrected_dedup_count.tsv"
         self.cell_qc_metrics = f"{self.analysis_dir}/cell_qc_metrics.tsv"
         self.peak_res =  f"{self.analysis_dir}/peak/{self.sample}_final_peaks.bed"
         self.df_tsne_file = f"{self.analysis_dir}/tsne_coord.tsv"
-        self.fragment = f"{self.analysis_dir}/fragments_corrected_dedup_count.tsv.gz*"
+        self.fragment_files = f"{self.analysis_dir}/fragments_corrected_dedup_count.tsv.gz*"
         self.out = f"{self.outdir}/../outs"
 
     @staticmethod
@@ -37,6 +42,33 @@ class Analysis(Step):
         else:
             return "Non-cells"
     
+    @utils.add_log
+    def run_snapatac2(self):
+        """run snapatac2"""
+
+        genome = snap.genome.Genome(
+            fasta=f'{self.reference}/genome.fa',
+            annotation=f'{self.reference}/gene.gtf'
+        )
+        data = snap.pp.import_data(
+            self.fragment,
+            chrom_sizes=genome,
+            min_num_fragments=0,
+            sorted_by_barcode=False,
+            chrM=["chrM", "M", "MT", "Mt"]
+        )
+        cell_barcode = utils.read_one_col(self.df_tsne_file)[0]
+        cell_barcode = [item.split('\t')[0] for item in cell_barcode]
+        data_cell = data[data.obs.index.isin(cell_barcode)]
+        snap.metrics.tsse(data_cell, genome)
+        data_cell.obs.tsse = data_cell.obs.tsse.astype(float)
+        median_tsse = round(np.median(data_cell.obs["tsse"]), 2)
+        self.add_metric(
+            name = 'Median of TSS enrichment score',
+            value = median_tsse,
+            help_info = 'Median of Transcription Start Site (TSS) Enrichment Score.'           
+        )
+
     @utils.add_log
     def add_metrics(self):
         """plot and add metrics.
@@ -70,18 +102,19 @@ class Analysis(Step):
     
     def cp_files(self):
         """copy files"""
-        files = [self.df_tsne_file, self.peak_res, self.cell_qc_metrics, f"{self.analysis_dir}/peak/*.h5", self.fragment]
+        files = [self.df_tsne_file, self.peak_res, self.cell_qc_metrics, f"{self.analysis_dir}/peak/*.h5", self.fragment_files]
         for file in files:
             cmd = f"cp {file} {self.outdir}"
             subprocess.check_call(cmd, shell=True)
         
         utils.check_mkdir(self.out)
-        files = [self.cell_qc_metrics, self.filtered_peak_count, self.fragment]
+        files = [self.cell_qc_metrics, self.filtered_peak_count, self.fragment_files]
         for file in files:
             cmd = f"cp {file} {self.out}"
             subprocess.check_call(cmd, shell=True)
             
     def run(self):
+        self.run_snapatac2()
         self.add_metrics()
         self.cp_files()
 
